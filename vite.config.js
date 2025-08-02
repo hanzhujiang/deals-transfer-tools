566a94df4675abfe22d5146b5efe4cab237d9978
Alan Han <alanhan@Alans-MacBook-Pro.local>
2025-08-02 21:13:18 +1000

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import rollupNodePolyFill from 'rollup-plugin-node-polyfills'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  resolve: {
+    alias: {
+      crypto: 'crypto-browserify',
+      stream: 'stream-browserify',
+      buffer: 'buffer',
+    },
+  },
+  build: {
+    rollupOptions: {
+      plugins: [rollupNodePolyFill()]
+    },
+  },
+  optimizeDeps: {
+    include: ['crypto-browserify', 'stream-browserify', 'buffer'],
+  }
 })
